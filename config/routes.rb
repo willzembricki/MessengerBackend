d84bbe283dec0beb5241070bd4879bbdfbbda2c3
William Zembricki <wzembricki@gmail.com>
2021-12-15 14:48:15 -0500

    associations and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  resources :messages
-  resources :users
-  resources :chatrooms
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  namespace :api do
+
+    namespace :v1 do 
+
+      post '/login', to 'auth#create'
+      resources :messages, only: [:index,:create]
+      resources :users, only: [:index, :create]  
+        member do 
+          get 'chatrooms'
+        end
+    end
+      resources :chatrooms, only: [:index,:create, :show]
+
+  end  
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+mount ActionCable.server => './cable'
